53735151afa564016da35a03fba43ba1c209a2a5
billiejoenahema <billiejoenahema@gmail.com>
2021-03-20 21:32:33 +0900

    create: nav link, add: nav&footer to some pages

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,6 @@ export default function Home() {
     return (
         <Layout title="Home">
             <p className="text-4xl">Welcome to Nextjs</p>
-
-            {/* <footer>
-                <a
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Powered by{' '}
-                    <img src="/vercel.svg" alt="Vercel Logo" />
-                </a>
-            </footer> */}
         </Layout>
     )
 }
