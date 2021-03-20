04884265530a38c11c908a332859c531b05be32a
billiejoenahema <billiejoenahema@gmail.com>
2021-03-20 20:58:11 +0900

    add: Layout wrapper

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,20 @@
-import Head from 'next/head'
+import Layout from '../components/Layout'
 
 export default function Home() {
     return (
-        <div className="min-h-screen py-0 px-2 flex flex-col justify-center items-center">
-            <Head>
-                <title>Create Next App</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <h1>Hello Nextjs</h1>
+        <Layout title="Home">
+            <p className="text-4xl">Welcome to Nextjs</p>
 
-
-            {/* <footer className={styles.footer}>
+            {/* <footer>
                 <a
                     href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
                     Powered by{' '}
-                    <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+                    <img src="/vercel.svg" alt="Vercel Logo" />
                 </a>
             </footer> */}
-        </div>
+        </Layout>
     )
 }
